e8a06b2149d379ba45a3ceb99e7fc6d6f831d863
Guillaume Patoine <guillaume.patoine@idiv.de>
2022-05-03 13:57:15 +0200

    lnv up2

---
+++ R/00-imports.R
@@ -0,0 +1,18 @@
+# https://stackoverflow.com/questions/57618769/how-to-automatically-load-functions-into-namespace-of-an-r-package
+
+
+#' re-export str_rev
+#'
+#' @importFrom stringi stri_reverse
+#' @name stri_reverse
+#' @rdname stri_reverse
+#' @export
+NULL
+
+
+# #' re-export str_rev
+# #'
+# #' @title str_rev
+# #' @rdname stri_reverse2
+# #' @export
+# str_rev <- stringi::stri_reverse
